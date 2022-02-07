32b37249846bd4c6be918976d461a72466f87056
Guy Dilena <guydilena@gmail.com>
2022-02-07 19:42:22 -0400

    new file added. tail.js to remove the last item of.  the array

---
+++ tail.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `👍👍👍${actual} === ${expected}`;
+  } else {
+    return `👎👎👎Assertion Failed: ${actual} !== ${expected}`;
+  }
+};
+
+const tail = function(array) {
+  let tailArray = array.slice(1);
+  return tailArray;
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+console.log(tail(words));
+console.log(assertEqual(words.length, 3));
