8b24acdbe9da7062577b24ca34b325432c7d2019
Guy Dilena <guydilena@gmail.com>
2022-02-07 18:35:56 -0400

    new assertEqual function with Emojis!

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `👍👍👍${actual} === ${expected}`;
+  } else {
+    return `👎👎👎Assertion Failed: ${actual} !== ${expected}`;
+  }
+};
+
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
+console.log(assertEqual(1021,1021));
+console.log(assertEqual("Human", "Hooman"));
+console.log(assertEqual(55,34));
