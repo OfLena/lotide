eebac998fb204d503f9e93edf914afa87039cfc7
Guy Dilena <guydilena@gmail.com>
2022-02-08 20:14:26 -0400

    new file eqArrays with the assertEqual function

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `👍👍👍${actual} === ${expected}`;
+  } else {
+    return `👎👎👎Assertion Failed: ${actual} !== ${expected}`;
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)); // => true
+console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false)); // => false
+
+console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true)); // => true
+console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)); // => false
+
