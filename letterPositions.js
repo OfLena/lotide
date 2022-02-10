79f4130dc07b7b8893f7fce51fb79d1a1e0d7053
Guy Dilena <guydilena@gmail.com>
2022-02-09 20:12:22 -0400

    new file letterPositions. letterPositions takes in a string and outputs an object of arrays listing the index position of each character.

---
+++ letterPositions.js
@@ -0,0 +1,37 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  const myString = sentence.replace(/\s/g, "");
+
+  for (let i = 0; i < myString.length; i++) {
+    let letter = myString[i];
+    if (results[letter]) {
+      results[letter].push(i);
+    } else {
+      results[letter] = [i];
+    }
+  }
+  return results;
+};
+
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  } else {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEq = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`👍👍👍${actual} === ${expected}`);
+  } else {
+    console.log(`👎👎👎Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertArraysEq(letterPositions("hello").e, [1]);
