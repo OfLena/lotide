7b8de4d28040c8d732d60e57c8f688b2481d3a7e
Guy Dilena <guydilena@gmail.com>
2022-02-14 08:22:09 -0400

    refactored tail and created a new tailTest file to test the function without test cases inside of tail.js

--- tail.js
+++ tail.js
@@ -1,16 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return `✅✅✅${actual} === ${expected}`;
-  } else {
-    return `❌❌❌Assertion Failed: ${actual} !== ${expected}`;
-  }
-};
+const assertEqual = require('./assertEqual');
+
 
 const tail = function(array) {
   let tailArray = array.slice(1);
   return tailArray;
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(words));
-console.log(assertEqual(words.length, 3));
+module.exports = tail;
